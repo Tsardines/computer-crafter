eb8e0b0c885049168b5fc21dd230c26529a71b3e
Haley Sulcer <hjsulcer@gmail.com>
2018-04-27 12:21:53 -0400

    started creating seed info and dummy data

--- computer-crafter/db/seeds.rb
+++ computer-crafter/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = {
+  haley: {
+    username: "haley",
+    password: "haley"
+  },
+  geralt: {
+    username: "geralt",
+    password: "geralt"
+  }
+}
+
+user_parts_list = {
+  test_list: {
+    username: "test_username",
+    cpu: "test_cpu",
+    cpu_cooler: "test_cpu_cooler",
+    mobo: "test_mobo",
+    ram: "test_ram",
+    storage: "test_storage",
+    case: "test_case",
+    power_supply: "test_power_supply",
+    vid_card: "test_vid_card",
+    monitor: "test_monitor"
+  }
+}
+
+mobos = {
+  gigabyte_ga: {
+    name:
+    socket:
+    form_factor:
+    ram_slots:
+    max_ram:
+    price:
+  }
+}
