abc5f2608d44b67e1142ff990fc27603d8ef76cb
Haley Sulcer <hjsulcer@gmail.com>
2018-05-01 09:53:39 -0400

    successfully created '126 specs and 27 parts'

--- computer-crafter/db/seeds.rb
+++ computer-crafter/db/seeds.rb
@@ -10,60 +10,106 @@ Part.destroy_all
 Spec.destroy_all
 
   Part.create([
-
     # ===== Cases ===== #
     { name: 'NZXT S340' },
     { name: 'Phantex Ethoo Pro M Tempered Glass' },
-    { name: 'Corsair Crystal 570X RGB' }
+    { name: 'Corsair Crystal 570X RGB' },
+
+    # ===== CPUs ===== #
+    { name: 'Intel Core i7-8700k' },
+    { name: 'AMD Ryzen 5 2600X'},
+    { name: 'Intel Core i7-7700k'},
+
+    # ===== CPU Coolers ===== #
+    { name: 'Cooler Master Hyper 212 EVO'},
+    { name: 'Corsair H100i v2 Liquid CPU Cooler'},
+    { name: 'NZXT Kraken X62 Rev 2'},
 
     # ===== Mobos ===== #
     { name: 'Gigabyte GA-H110M-A' },
     { name: 'MSI B250M PRO-VD' },
-    { name: 'Asus Prime Z270-A'}
+    { name: 'Asus Prime Z270-A'},
+
+    # ===== Monitors ===== #
+    { name: 'Acer GN246HL' },
+    { name: 'Asus VP228H' },
+    { name: 'Dell S2417DG' },
+
+    # ===== PSUs ===== #
+    { name: 'EVGA SuperNOVA 550' },
+    { name: 'Seasonic M1211 520 Bronze' },
+    { name: 'Corsair CX450M' },
+
+    # ===== RAM ===== #
+    { name: 'G.Skill Ripjaws V Series' },
+    { name: 'Corsair Vengeance LPX' },
+    { name: 'Team Vulcan' },
+
+    # ===== Storage ===== #
+    { name: 'Western Digital WD10EZEX' },
+    { name: 'Seagate ST2000DM006' },
+    { name: 'Samsung MZ-75E250B/AM' },
+
+    # ===== Vid Cards ===== #
+    { name: 'EVGA 06G-P4-6163-KR' },
+    { name: 'Asus ROG STRIX-GTX1080-8G-GAMING' },
+    { name: 'MSI RX 560 AERO ITX 4G OC' }
   ])
 
   Spec.create([
-
+    # ===== Cases ===== #
     { attribute_name: 'Color', attribute_value: 'black', part_id: 4 },
     { attribute_name: 'Ext', attribute_value: 3, part_id: 4 },
     { attribute_name: 'Int', attribute_value: 2, part_id: 4 },
     { attribute_name: 'Power Supply', attribute_value: false, part_id: 4 },
-    { attribute_name: 'Type', attribute_value: 32, part_id: 4 },
-    { attribute_name: 'Price', attribute_value: 35.88, part_id: 4 },
+    { attribute_name: 'Type', attribute_value: 'ATX Mid Tower', part_id: 4 },
+    { attribute_name: 'Price', attribute_value: 54.99, part_id: 4 },
 
     { attribute_name: 'Color', attribute_value: 'black', part_id: 5 },
     { attribute_name: 'Ext', attribute_value: 1, part_id: 5 },
     { attribute_name: 'Int', attribute_value: 3, part_id: 5 },
-    { attribute_name: 'Power Supply', attribute_value: 32, part_id: 5 },
-    { attribute_name: 'Type', attribute_value: 32, part_id: 5 },
-    { attribute_name: 'Price', attribute_value: 45.89, part_id: 5 },
+    { attribute_name: 'Power Supply', attribute_value: false, part_id: 5 },
+    { attribute_name: 'Type', attribute_value: 'ATX Mid Tower', part_id: 5 },
+    { attribute_name: 'Price', attribute_value: 79.99, part_id: 5 },
 
     { attribute_name: 'Color', attribute_value: 'black', part_id: 6 },
     { attribute_name: 'Ext', attribute_value: 2, part_id: 6 },
     { attribute_name: 'Int', attribute_value: 2, part_id: 6 },
-    { attribute_name: 'Power Supply', attribute_value: 64, part_id: 6 },
-    { attribute_name: 'Type', attribute_value: 32, part_id: 6 },
-    { attribute_name: 'Price', attribute_value: 299, part_id: 6 },
+    { attribute_name: 'Power Supply', attribute_value: false, part_id: 6 },
+    { attribute_name: 'Type', attribute_value: 'ATX Mid Tower', part_id: 6 },
+    { attribute_name: 'Price', attribute_value: 144.67, part_id: 6 },
 
+    # ===== CPUs ===== #
+    { attribute_name: 'Socket', attribute_value: 'LGA1151', part_id: 7 },
+    { attribute_name: 'Speed', attribute_value: 3.7, part_id: 7 },
+    { attribute_name: 'Cores', attribute_value: 6, part_id: 7 },
+    { attribute_name: 'TDP', attribute_value: 95, part_id: 7 },
+    { attribute_name: 'Price', attribute_value: 339.79, part_id: 7 },
 
-    #     int: 2,
-    #     power_supp: false,
-    #     price: 54.99,
-    #     type: "ATX Mid Tower",
-    #   },
+    { attribute_name: 'Socket', attribute_value: 'AM4', part_id: 8 },
+    { attribute_name: 'Speed', attribute_value: 3.6, part_id: 8 },
+    { attribute_name: 'Cores', attribute_value: 6, part_id: 8 },
+    { attribute_name: 'TDP', attribute_value: 95, part_id: 8 },
+    { attribute_name: 'Price', attribute_value: 228.89, part_id: 8 },
 
-    #     int: 3,
-    #     power_supp: false,
-    #     price: 79.99,
-    #     type: "ATX Mid Tower",
-    #   },
+    { attribute_name: 'Socket', attribute_value: 'LGA1151', part_id: 9 },
+    { attribute_name: 'Speed', attribute_value: 4.2, part_id: 9 },
+    { attribute_name: 'Cores', attribute_value: 4, part_id: 9 },
+    { attribute_name: 'TDP', attribute_value: 91, part_id: 9 },
+    { attribute_name: 'Price', attribute_value: 307.49, part_id: 9 },
 
-    #     int: 2,
-    #     power_supp: false,
-    #     price: 144.67,
-    #     type: "ATX Mid Tower",
-    #   },
-    # }
+    # ===== CPU COOLERS ===== #
+    { attribute_name: 'Fan RPM', attribute_value: 600-2000, part_id: 10 },
+    { attribute_name: 'Noise Level', attribute_value: 9.0-36.0, part_id: 10 },
+    { attribute_name: 'Price', attribute_value: 19.99, part_id: 10 },
+
+    { attribute_name: 'Fan RPM', attribute_value: 2435, part_id: 11 },
+    { attribute_name: 'Noise Level', attribute_value: 37.7, part_id: 11 },
+    { attribute_name: 'Price', attribute_value: 94.99, part_id: 11 },
+
+    { attribute_name: 'Fan RPM', attribute_value: 500-1800, part_id: 12 },
+    { attribute_name: 'Noise Level', attribute_value: 21.0-38.0, part_id: 12 },
+    { attribute_name: 'Price', attribute_value: 147.39, part_id: 12 },
 
     # ===== Mobos ===== #
     { attribute_name: 'Socket', attribute_value: 'LGA1151', part_id: 1 },
@@ -83,6 +129,93 @@ Spec.destroy_all
     { attribute_name: 'RAM Slots', attribute_value: 4, part_id: 3 },
     { attribute_name: 'Max RAM', attribute_value: 64, part_id: 3 },
     { attribute_name: 'Price', attribute_value: 299, part_id: 3 },
+
+    # ===== Monitors ===== #
+    { attribute_name: 'Resolution', attribute_value: '1920x1080', part_id: 13 },
+    { attribute_name: 'Size', attribute_value: 24, part_id: 13 },
+    { attribute_name: 'Response Time', attribute_value: 1, part_id: 13 },
+    { attribute_name: 'IPS', attribute_value: false, part_id: 13 },
+    { attribute_name: 'Price', attribute_value: 190, part_id: 13 },
+
+    { attribute_name: 'Resolution', attribute_value: '1920x1080', part_id: 14 },
+    { attribute_name: 'Size', attribute_value: 21.5, part_id: 14 },
+    { attribute_name: 'Response Time', attribute_value: 444, part_id: 14 },
+    { attribute_name: 'IPS', attribute_value: false, part_id: 14 },
+    { attribute_name: 'Price', attribute_value: 99, part_id: 14 },
+
+    { attribute_name: 'Resolution', attribute_value: '2560x1440', part_id: 15 },
+    { attribute_name: 'Size', attribute_value: 23.8, part_id: 15 },
+    { attribute_name: 'Response Time', attribute_value: 1, part_id: 15 },
+    { attribute_name: 'IPS', attribute_value: false, part_id: 15 },
+    { attribute_name: 'Price', attribute_value: 369, part_id: 15 },
+
+    # ===== PSUs ===== #
+    { attribute_name: 'Type', attribute_value: 'ATX', part_id: 16 },
+    { attribute_name: 'Efficiency', attribute_value: 80, part_id: 16 },
+    { attribute_name: 'Fans', attribute_value: 1, part_id: 16 },
+    { attribute_name: 'Watts', attribute_value: 550, part_id: 16 },
+    { attribute_name: 'Modular', attribute_value: 'full', part_id: 16 },
+    { attribute_name: 'Price', attribute_value: 75.88, part_id: 16 },
+
+    { attribute_name: 'Type', attribute_value: 'ATX12V / EPS12V', part_id: 17 },
+    { attribute_name: 'Efficiency', attribute_value: 80, part_id: 17 },
+    { attribute_name: 'Fans', attribute_value: 1, part_id: 17 },
+    { attribute_name: 'Watts', attribute_value: 520, part_id: 17 },
+    { attribute_name: 'Modular', attribute_value: 'full', part_id: 17 },
+    { attribute_name: 'Price', attribute_value: 34.99, part_id: 17 },
+
+    { attribute_name: 'Type', attribute_value: 'ATX', part_id: 18 },
+    { attribute_name: 'Efficiency', attribute_value: 80, part_id: 18 },
+    { attribute_name: 'Fans', attribute_value: 1, part_id: 18 },
+    { attribute_name: 'Watts', attribute_value: 450, part_id: 18 },
+    { attribute_name: 'Modular', attribute_value: 'semi', part_id: 18 },
+    { attribute_name: 'Price', attribute_value: 26.99, part_id: 18 },
+
+    # ===== RAM ===== #
+    { attribute_name: 'CAS', attribute_value: 15, part_id: 19 },
+    { attribute_name: 'Modules', attribute_value: '2x8GB', part_id: 19 },
+    { attribute_name: 'Size', attribute_value: 16, part_id: 19 },
+    { attribute_name: 'Speed', attribute_value: 'DDR4-3000', part_id: 19 },
+    { attribute_name: 'Type', attribute_value: '288-pin DIMM', part_id: 19 },
+    { attribute_name: 'Price/gb', attribute_value: 11.25, part_id: 19 },
+    { attribute_name: 'Price', attribute_value: 179.99, part_id: 19 },
+
+    { attribute_name: 'CAS', attribute_value: 16, part_id: 20 },
+    { attribute_name: 'Modules', attribute_value: '2x8GB', part_id: 20 },
+    { attribute_name: 'Size', attribute_value: 16, part_id: 20 },
+    { attribute_name: 'Speed', attribute_value: 'DDR4-2400', part_id: 20 },
+    { attribute_name: 'Type', attribute_value: '288-pin DIMM', part_id: 20 },
+    { attribute_name: 'Price/gb', attribute_value: 10.49, part_id: 20 },
+    { attribute_name: 'Price', attribute_value: 167.89, part_id: 20 },
+
+    { attribute_name: 'CAS', attribute_value: 14, part_id: 21 },
+    { attribute_name: 'Modules', attribute_value: '2x8GB', part_id: 21 },
+    { attribute_name: 'Size', attribute_value: 16, part_id: 21 },
+    { attribute_name: 'Speed', attribute_value: 'DDR4-2400', part_id: 21 },
+    { attribute_name: 'Type', attribute_value: '288-pin DIMM', part_id: 21 },
+    { attribute_name: 'Price/gb', attribute_value: 9.37, part_id: 21 },
+    { attribute_name: 'Price', attribute_value: 149.99, part_id: 21 },
+
+    # ===== STORAGE ===== #
+    { attribute_name: 'Series', attribute_value: 'SC GAMING', part_id: 22 },
+    { attribute_name: 'Chipset', attribute_value: 3.5, part_id: 22 },
+    { attribute_name: 'Memory', attribute_value: 6, part_id: 22 },
+    { attribute_name: 'Core Clock', attribute_value: 1.61, part_id: 22 },
+    { attribute_name: 'Price', attribute_value: 299.99, part_id: 22 },
+
+    { attribute_name: 'Series', attribute_value: 'STRIX', part_id: 23 },
+    { attribute_name: 'Chipset', attribute_value: 3.5, part_id: 23 },
+    { attribute_name: 'Memory', attribute_value: 8, part_id: 23 },
+    { attribute_name: 'Core Clock', attribute_value: 1.61, part_id: 23 },
+    { attribute_name: 'Price', attribute_value: 599.99, part_id: 23 },
+
+    { attribute_name: 'Series', attribute_value: 'AERO ITX', part_id: 24 },
+    { attribute_name: 'Chipset', attribute_value: 2.5, part_id: 24 },
+    { attribute_name: 'Memory', attribute_value: 4, part_id: 24 },
+    { attribute_name: 'Core Clock', attribute_value: 1.20, part_id: 24 },
+    { attribute_name: 'Price', attribute_value: 134.99, part_id: 24 },
+    ####### keep last comma?
+
   ])
 
 
@@ -90,17 +223,7 @@ puts "Created #{Spec.count} specs and #{Part.count} parts"
 
 
 
-# Make a new Vid Card
-# vidcard = Part.create({name: 'vidcard'})
-# spec1 = Spec.create({attribute_name: 'Series', attribute_value: '4800'})
-# spec2 = Spec.create({attribute_name: 'Memory', attribute_value: '16'})
-# spec3 = Spec.create({attribute_name: 'Chipset', attribute_value: 'intel'})
-# spec4 = Spec.create({attribute_name: 'Coreclock', attribute_value: '150'})
-# vidcard.specs << spec1
-# vidcard.specs << spec2
-# vidcard.specs << spec3
-# vidcard.specs << spec4
-# vidcard.save
+
 
 
 
@@ -131,184 +254,13 @@ puts "Created #{Spec.count} specs and #{Part.count} parts"
 # }
 #
 
-#
-# cpus = {
-#   int_87k: {
-#     name: "Intel Core i7-8700k",
-#     socket: "LGA1151",
-#     speed: 3.7,
-#     cores: 6,
-#     tdp: 95,
-#     price: 339.79,
-#   },
-#   amd_ry5: {
-#     name: "AMD Ryzen 5 2600X",
-#     socket: "AM4",
-#     speed: 3.6,
-#     cores: 6,
-#     tdp: 95,
-#     price: 228.89,
-#   },
-#   int_77k: {
-#     name: "Intel Core i7-7700k",
-#     socket: "LGA1151",
-#     speed: 4.2,
-#     cores: 4,
-#     tdp: 91,
-#     price: 307.49,
-#   }
-# }
-#
-# cpu_coolers = {
-#   co_ma_hyp: {
-#     name: "Cooler Master Hyper 212 EVO",
-#     fan_rpm: 600-2000,
-#     noise_level: 9.0-36.0,
-#     price: 19.99,
-#   },
-#   cors_h: {
-#     name: "Corsair H100i v2 Liquid CPU Cooler",
-#     fan_rpm: 2435,
-#     noise_level: 37.7,
-#     price: 94.99,
-#   },
-#   nzxt_kra: {
-#     name: "NZXT Kraken X62 Rev 2",
-#     fan_rpm: 500-1800,
-#     noise_level: 21.0-38.0,
-#     price: 147.39,
-#   }
-# }
-#
 
-#
-# monitors = {
-#   acer_gn: {
-#     name: "Acer GN246HL",
-#     resolution: "1920x1080",
-#     size: 24,
-#     resp_time: 1,
-#     ips: false,
-#     price: 190,
-#   },
-#   asus_vp: {
-#     name: "Asus VP228H",
-#     resolution: "1920x1080",
-#     size: 21.5,
-#     resp_time: 1,
-#     ips: false,
-#     price: 99,
-#   },
-#   acer_gn: {
-#     name: "Dell S2417DG",
-#     resolution: "2560x1440",
-#     size: 23.8,
-#     resp_time: 1,
-#     ips: false,
-#     price: 369,
-#   }
-# }
-#
-# power_supplies = {
-#   evga_550: {
-#     name: "EVGA SuperNOVA 550",
-#     type: "ATX",
-#     efficiency: 80,
-#     fans: 1,
-#     watts: 550,
-#     modular: "full",
-#     price: 75.88,
-#   },
-#   sea_m12: {
-#     name: "Seasonic M1211 520 Bronze",
-#     type: "ATX12V / EPS12V",
-#     efficiency: 80,
-#     fans: 1,
-#     watts: 520,
-#     modular: "full",
-#     price: 34.99,
-#   },
-#   cors_cx: {
-#     name: "Corsair CX450M ",
-#     type: "ATX",
-#     efficiency: 80,
-#     fans: 1,
-#     watts: 450,
-#     modular: "semi",
-#     price: 26.99,
-#   }
-# }
-#
-# rams = {
-#   gsk_rip: {
-#     name: "G.Skill Ripjaws V Series",
-#     cas: 15,
-#     modules: "2x8GB",
-#     size: 16,
-#     speed: "DDR4-3000",
-#     type: "288-pin DIMM",
-#     price_gb: 11.25,
-#     price: 179.99,
-#   },
-#   cors_veng_lpx: {
-#     name: "Corsair Vengeance LPX",
-#     cas: 16,
-#     modules: "2x8GB",
-#     size: 16,
-#     speed: "DDR4-2400",
-#     type: "288-pim DIMM",
-#     price_gb: 10.49,
-#     price: 167.89,
-#   },
-#   team_vulc: {
-#     name: "Team Vulcan",
-#     cas: 14,
-#     modules: "2x8GB",
-#     size: 16,
-#     speed: "DDR4-2400",
-#     type: "288-pm DIMM",
-#     price_gb: 9.37,
-#     price: 149.99,
-#   }
-# }
-#
-# storages = {
-#   wd_wd10: {
-#     name: "Western Digital WD10EZEX",
-#     series: "Caviar Blue",
-#     form: 3.5,
-#     type: "7200RPM",
-#     capacity: "1TB",
-#     cache: 64,
-#     price_gb: 0.044,
-#     price: 43.85,
-#   },
-#   sgte_st2: {
-#     name: "Seagate ST2000DM006",
-#     series: "Barracuda",
-#     form: 3.5,
-#     type: "7200RPM",
-#     capacity: "2TB",
-#     cache: 64,
-#     price_gb: 0.030,
-#     price: 59.79,
-#   },
-#   sams_mz: {
-#     name: "Samsung MZ-75E250B/AM",
-#     series: "850 EVO-Series",
-#     form: 2.5,
-#     type: "SSD",
-#     capacity: "250GB",
-#     cache: "N/A",
-#     price_gb: 0.400,
-#     price: 99.99,
-#   }
-# }
+
 #
 # vid_cards = {
 #   evga_06g: {
 #     name: "EVGA 06G-P4-6163-KR",
-#     seires: "SC GAMING",
+#     series: "SC GAMING",
 #     chipset: "GeForce GTX 1060 6GB",
 #     memory: 6,
 #     core_clock: 1.61,
@@ -316,7 +268,7 @@ puts "Created #{Spec.count} specs and #{Part.count} parts"
 #   },
 #   asus_strix: {
 #     name: "Asus ROG STRIX-GTX1080-8G-GAMING",
-#     seires: "STRIX",
+#     series: "STRIX",
 #     chipset: "GeForce GTX 1080",
 #     memory: 8,
 #     core_clock: 1.61,
@@ -324,7 +276,7 @@ puts "Created #{Spec.count} specs and #{Part.count} parts"
 #   },
 #   msi_560: {
 #     name: "MSI RX 560 AERO ITX 4G OC",
-#     seires: "AERO ITX",
+#     series: "AERO ITX",
 #     chipset: "Radeon RX 560 - 1024",
 #     memory: 4,
 #     core_clock: 1.20,
