98b6e562367db636d3d90ccfb0702af774181925
Haley Sulcer <hjsulcer@gmail.com>
2018-05-02 14:21:26 -0400

    sign in endpoint changed from /users/sign_up to /signup

--- computer-crafter/config/routes.rb
+++ computer-crafter/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   # displays main page
   root 'pages#index'
   resources :parts do
